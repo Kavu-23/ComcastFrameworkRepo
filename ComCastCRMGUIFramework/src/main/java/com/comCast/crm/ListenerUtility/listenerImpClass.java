406e3c17a983316026fc2380b09a9391dc7ecfae
DELL <DELL@DESKTOP-5DJG0VO>
2024-05-18 10:07:56 +0530

    POM added

--- ComCastCRMGUIFramework/src/main/java/com/comCast/crm/ListenerUtility/listenerImpClass.java
+++ ComCastCRMGUIFramework/src/main/java/com/comCast/crm/ListenerUtility/listenerImpClass.java
@@ -1,27 +1,20 @@
 package com.comCast.crm.ListenerUtility;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Date;
 
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-import com.comcas.crm.baseTest.BaseClass;
-import com.comcas.crm.baseTest.BaseClass2;
 
 import crm.generic.webDriverUtility.UtilityClassObject;
 
